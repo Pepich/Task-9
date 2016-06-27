38dea23750dcd5006ec5158256b37dc66518985a
Pepich <benedikt.abel@yahoo.de>
2016-06-27 10:00:15 +0200

    Fixed comments

--- B-19/quad.c
+++ B-19/quad.c
@@ -14,8 +14,10 @@
 int quad(double a, double b, double c, double *r_1, double *r_2)
 {
     float d = b*b-4*a*c;
+    // Returning logical false when no solutions are to be found or when it is not a quadratic equasion
     if (d < 0 || a == 0)
 	return 0;
+    // Calculating the solutions
     *r_1 = (-b+sqrt(d))/(4*a);
     *r_2 = (-b-sqrt(d))/(4*a);
     return 1;
@@ -23,12 +25,15 @@ int quad(double a, double b, double c, double *r_1, double *r_2)
 int main(void)
 {
     double a = 0, b = 0, c = 0, r_1 = 0, r_2 = 0;
+    // Scanning values
     printf("Bitte a eingeben: ");
     scanf("%lf",&a);
     printf("Bitte b eingeben: ");
     scanf("%lf",&b);
     printf("Bitte c eingeben: ");
     scanf("%lf",&c);
+    
+    // Doing the work, outputting solutions, or if none to be found, outputting error message
     if (quad(a, b, c, &r_1, &r_2))
 	printf("Die Lösungen der quadratischen Gleichung lauten %f; %f.\n", r_1, r_2);
     else
